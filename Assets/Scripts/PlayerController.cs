b90ff346d595da326396336271e9ef9e3b8f726c
krashyd <a52441@alunos.ipb.pt>
2024-06-24 18:46:53 +0100

    checking heath adn collectibles
    A52441

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,13 @@ public class PlayerController : MonoBehaviour
   public InputAction MoveAction;
   Rigidbody2D rigidbody2d;
   Vector2 move;
+  public float speed = 3.0f;
+
+
+  // Variables related to the health system
+  public int maxHealth = 5;
+  public int health { get { return currentHealth; }}
+  int currentHealth = 1;
 
 
   // Start is called before the first frame update
@@ -23,13 +30,21 @@ public class PlayerController : MonoBehaviour
   void Update()
   {
      move = MoveAction.ReadValue<Vector2>();
-     Debug.Log(move);
   }
 
 
+  // FixedUpdate has the same call rate as the physics system
   void FixedUpdate()
   {
-     Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+     Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
      rigidbody2d.MovePosition(position);
   }
+
+
+  public void ChangeHealth (int amount)
+  {
+     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+     Debug.Log(currentHealth + "/" + maxHealth);
+  }
+
 }
