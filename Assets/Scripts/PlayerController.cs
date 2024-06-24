52d14e0139c4fc06df827bedd1ec4d5decd23ef3
krashyd <a52441@alunos.ipb.pt>
2024-06-24 18:27:04 +0100

    ambiente do jogo e fisica
    a52441

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+  // Variables related to player character movement
   public InputAction MoveAction;
+  Rigidbody2D rigidbody2d;
+  Vector2 move;
 
 
   // Start is called before the first frame update
   void Start()
   {
-     MoveAction.Enable();   
+     MoveAction.Enable();
+     rigidbody2d = GetComponent<Rigidbody2D>();
   }
  
   // Update is called once per frame
   void Update()
   {
-     Vector2 move = MoveAction.ReadValue<Vector2>();
+     move = MoveAction.ReadValue<Vector2>();
      Debug.Log(move);
-      Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-     transform.position = position;
   }
 
 
+  void FixedUpdate()
+  {
+     Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+     rigidbody2d.MovePosition(position);
+  }
 }
