fb52f283bffa4a114dbb738452a0c29a184170c2
krashyd <a52441@alunos.ipb.pt>
2024-06-24 17:55:45 +0100

    new tiles
    a52441

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-   public InputAction MoveAction;
+  public InputAction MoveAction;
 
 
-   // Start is called before the first frame update
-   void Start()
-   {
-      MoveAction.Enable();  
-   }
-
-
-   // Update is called once per frame
-   void Update()
-   {
+  // Start is called before the first frame update
+  void Start()
+  {
+     MoveAction.Enable();   
+  }
+ 
+  // Update is called once per frame
+  void Update()
+  {
      Vector2 move = MoveAction.ReadValue<Vector2>();
      Debug.Log(move);
-     Vector2 position = (Vector2)transform.position + move * 0.1f;
+      Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
      transform.position = position;
-   }
+  }
+
+
 }
